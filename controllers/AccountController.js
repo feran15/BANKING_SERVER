1f72be3e6440655009efd4c9dad9a1cc6fee9fc8
feran15 <abodundepaul2022@gmail.com>
2025-07-02 15:26:40 +0100

    ...

--- controllers/AccountController.js
+++ controllers/AccountController.js
@@ -1,1 +1,23 @@
 const express = require("express")
+const AppError = require ("../utils/AppEror")
+const Account = require("../model/Account")
+
+// Get all client's details
+const getAllBankAccount = async (req, res, next) => {
+    try{
+        const allBankAccount = await Account.find();
+        res.json({
+              status:"Success",
+            message:"All Bank accounts fetched successfully",
+            result:allBankAccount.length,
+            data: allBankAccount
+        })
+    } catch (error) {
+        throw new AppError("Something went Wrong", 400)
+    }
+}
+
+// Create a new Account
+    const newAccount =  async (req, res) => {
+        
+    }
