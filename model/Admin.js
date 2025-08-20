fd9b41a991319a81c8393218473749a4484ba193
feran15 <abodundepaul2022@gmail.com>
2025-08-20 23:39:17 +0100

    Added Admin Control

--- model/Admin.js
+++ model/Admin.js
@@ -1,1 +1,24 @@
 const mongoose = require('mongoose')
+const { applyTimestamps } = require('./TransactionPin')
+
+const adminSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    Password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:["Superadmin","Staff"], 
+        default:"Staff"
+    }
+},{timestamps:"true"})
