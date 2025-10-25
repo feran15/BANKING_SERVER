cddc32e1d171e7b8eeae9b2aff762faf21946a60
feran15 <abodundepaul2022@gmail.com>
2025-10-25 14:56:25 +0100

    User transaction Pin

--- Server.js
+++ Server.js
@@ -8,6 +8,7 @@ const DashboardRoutes = require("./routes/Dashboard.js");
 const TransactionRoutes = require("./routes/TransactionRoutes.js");
 // const PaymentRoutes = require("./routes/PaymentModalRoutes.js");
 const AIInsightsRoutes = require("./routes/InsightRoutes.js");
+const TransactionPinRoutes = require("./routes/TransactionPinRoutes.js");
 
 
 
@@ -62,7 +63,7 @@ app.use("/api/dashboard", DashboardRoutes);
 app.use("/api/transactions", TransactionRoutes);
 // app.use("/api/payments", PaymentRoutes);
 app.use("/ai/insights", AIInsightsRoutes);
-
+app.use("/api/transaction-pin", TransactionPinRoutes)
 // ✅ Root endpoint
 app.get("/", (req, res) => {
   res.send("🚀 Banking API is running...");
--- routes/TransactionPinRoutes.js
+++ routes/TransactionPinRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const router = express.Router();
-const AuthController = require("../controllers/authController");
+const AuthController = require("../controller/authController");
 const verifyToken = require("../middleware/auth");
 
 router.post("/set-pin", verifyToken, AuthController.setTransactionPin);
