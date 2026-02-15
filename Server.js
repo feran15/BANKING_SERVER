d8d3ca045870102f9f5c06b6a3689ce47c275e7a
feran15 <abodundepaul2022@gmail.com>
2026-02-15 11:38:12 +0100

    Mongo Deployment

--- Server.js
+++ Server.js
@@ -54,9 +54,16 @@ app.get("/test-cors", (req, res) => {
 
 // ✅ MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("✅ Connected to MongoDB"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+  .catch((err) => {
+    console.error("❌ MongoDB connection error:", err.message);
+    process.exit(1);
+  });
+
 
 // ✅ Mount Routes
 app.use("/api/User", UserRoutes);
