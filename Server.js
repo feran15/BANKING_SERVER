69f40d09254888737a10e2233a6c1f646ae595c1
feran15 <abodundepaul2022@gmail.com>
2026-02-15 06:02:09 +0100

    App Error

--- Server.js
+++ Server.js
@@ -71,7 +71,7 @@ app.get("/", (req, res) => {
 });
 
 // ✅ Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
